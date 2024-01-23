c32df66dc4fe3b8ed0f165531413b5bba712b214
Unique-chiemerie <lilanimate1@gmail.com>
2024-01-23 23:44:33 +0100

    testing for data type notation

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("confrence is %T, remaining tickets is %T, conferencename is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have a total of %v, tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Book tickets now")
-	var userName = "something"
+
+	var userName string
+	var userTickets int
 	//asking the user their name
 
-	fmt.Println(userName)
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("user %v booked %v tickets\n", userName, userTickets)
 
 }

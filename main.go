f7b95aae210b9a3c6a7b01bf3e19a72af4952281
Unique-chiemerie <lilanimate1@gmail.com>
2024-01-24 00:37:17 +0100

    working with arrays

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	fmt.Printf("we have a total of %v, tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Book tickets now")
 
+	var bookings [50]string
 	var firstName string
 	var lastName string
 	var email string
@@ -28,6 +29,11 @@ func main() {
 	fmt.Println("Enter your number of Tickets to get:")
 	fmt.Scan(&userTickets)
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+	fmt.Printf("The whole array : %v\n", bookings)
+	fmt.Printf("The first value : %T\n", bookings[0])
+	fmt.Printf("Array type : %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 

7b3931909d47136ef93a82fe94cb0462a0a2ec1d
Unique-chiemerie <lilanimate1@gmail.com>
2024-01-23 21:16:55 +0100

    Trying to get dynamic variable delaration

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have a total of %v, tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Book tickets now")
+	var userName = "something"
+	//asking the user their name
+
+	fmt.Println(userName)
 
 }
